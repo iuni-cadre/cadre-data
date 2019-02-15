9999bf1969cba608aede75861b755bc9680dc0a2
Aditya Gupta <contactadityag@gmail.com>
2019-02-15 15:46:25 -0500

    I am just making changes to the fallback status of the Cadre Data API.

--- middleware/app.py
+++ middleware/app.py
@@ -11,19 +11,14 @@ def welcome():
 @app.route('/status')
 def status():
     # this route should return the status of the data API.
-    return jsonify({'Status': 'The API is running if the API is running.'}), 404
+    return jsonify({'Status': 'The API is running if the API is running.'}), 200
 
 
+@app.route('/<path:fallback>')
 @app.route('/api/<path:fallback>')
 def api_fallback(fallback):
     # this route should catch all api calls that aren't actually endpoints
     return jsonify({'error': 'Unknown Endpoint'}), 404
-
-
-@app.route('/<path:fallback>')
-def fallback(fallback):
-    # this function is defining what to display during a fallback
-    return render_template("fallback.html")
 
 
 if __name__ == '__main__':
