75df3ddcb7efb246e8978dfcb8400fedcb94f373
Aditya Gupta <contactadityag@gmail.com>
2019-02-15 15:26:43 -0500

    I am just adding the status and fallback end points to the data API.

--- middleware/app.py
+++ middleware/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -10,7 +10,20 @@ def welcome():
 
 @app.route('/status')
 def status():
-    return jsonify({'Status': 'The API is running'})
+    # this route should return the status of the data API.
+    return jsonify({'Status': 'The API is running if the API is running.'}), 404
+
+
+@app.route('/api/<path:fallback>')
+def api_fallback(fallback):
+    # this route should catch all api calls that aren't actually endpoints
+    return jsonify({'error': 'Unknown Endpoint'}), 404
+
+
+@app.route('/<path:fallback>')
+def fallback(fallback):
+    # this function is defining what to display during a fallback
+    return render_template("fallback.html")
 
 
 if __name__ == '__main__':
