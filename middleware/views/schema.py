fce8cfe779a000893e77963508aab35d9998057e
cpelikan <cpelikan@iu.edu>
2019-07-16 14:48:53 -0400

    update wos to use db from ec2 instance

--- middleware/views/schema.py
+++ middleware/views/schema.py
@@ -94,7 +94,7 @@ class Query(graphene.ObjectType):
                                       'authors_id_dais, authors_id_research, authors_prefix, authors_first_name, authors_middle_name, ' \
                                       'authors_last_name, authors_suffix, authors_initials, authors_display_name, authors_wos_name, ' \
                                       'authors_id_lang, authors_email, "references", issn, doi, title, journal_name, journal_abbrev, ' \
-                                      'journal_iso, abstract_paragraphs FROM interface_table WHERE '
+                                      'journal_iso, abstract_paragraphs FROM wos_core.interface_table WHERE '
                     for item in query_json:
                         logger.info(item)
                         field = item['field']
